3b203c3f9bb7e9aa435c7dd5b3e68fcda1fac02a
rking545789 <rking545789@gmail.com>
2025-12-18 11:11:25 +0800

    Add canonical tags
    Added a CanonicalTag component to manage canonical URLs.

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 import ScrollToTop from "@/components/ScrollToTop";
 import Index from "./pages/Index";
 import StartHere from "./pages/StartHere";
@@ -14,8 +14,25 @@ import ArticleDetail from "./pages/ArticleDetail";
 import PrivacyPolicy from "./pages/PrivacyPolicy";
 import TermsOfService from "./pages/TermsOfService";
 import NotFound from "./pages/NotFound";
+import { Helmet } from "react-helmet-async";
+
 
 const queryClient = new QueryClient();
+const SITE = "https://themodediary.com";
+
+function CanonicalTag() {
+  const location = useLocation();
+
+  const cleanPath = (location.pathname || "/").replace(/\/+$/, "") || "/";
+  const canonical = `${SITE}${cleanPath}`;
+
+  return (
+    <Helmet>
+      <link rel="canonical" href={canonical} />
+    </Helmet>
+  );
+}
+
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
@@ -23,6 +40,7 @@ const App = () => (
       <Toaster />
       <Sonner />
       <BrowserRouter>
+        <CanonicalTag />
         <ScrollToTop />
         <Routes>
           <Route path="/" element={<Index />} />
